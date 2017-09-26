17cfc23d34267cd6ec2175d022ca821a640f46b3
Jakub Vančata <vancata.kuba@seznam.cz>
2017-09-26 09:55:56 +0200

    Done

--- Pisemka1/Program.cs
+++ Pisemka1/Program.cs
@@ -8,9 +8,72 @@ namespace Pisemka1
 {
     class Program
     {
+        public static bool isNumberOrNot(char znak)
+        {           
+            double cislo = Char.GetNumericValue(znak);
+
+            if (cislo >= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }          
+        }
         static void Main(string[] args)
         {
-            int i = 1;
+            //List<double> output = new List<double>(); Jsem si neuvědomil, že pak ty čísla nemusím vypisovat XD
+            bool lastCheck = false; //True = poslední bylo číslo, False = poslední nebylo číslo            
+            double currentNumber = 0;            
+            int numCount = 0;
+            while (true)
+            {
+                while (true)
+                {
+                    string input = Console.ReadLine();
+                    if (String.IsNullOrEmpty(input))
+                    {
+                        Console.WriteLine("Musíš něco zadat, jinak z toho nemůžu zjistit, kolik je tam čísel.");
+                        continue;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < input.Length; i++)
+                        {
+                            //double cislo = Char.GetNumericValue();
+
+                            if (isNumberOrNot(input[i]))
+                            {
+                                lastCheck = true;
+                                currentNumber = currentNumber * 10 + Char.GetNumericValue(input[i]);
+                            }
+                            else
+                            {
+                                if (lastCheck && !String.IsNullOrEmpty(currentNumber.ToString()))
+                                {
+                                    //output.Add(currentNumber);
+                                    lastCheck = false;
+                                    currentNumber = 0;
+                                    numCount++;
+                                }
+                            }
+
+                        }
+                        if (Char.GetNumericValue(input[input.Length - 1]) >= 0)
+                        {
+                            numCount++;
+                        }
+                        Console.WriteLine("V textu je " + numCount + " čísel");
+                        break;
+                    }
+                }
+                //end of second while
+                currentNumber = 0;
+                numCount = 0;
+                lastCheck = false;
+            }
+            //end of first while
         }
     }
 }
